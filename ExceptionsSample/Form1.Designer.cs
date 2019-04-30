f0a56111b55d653e813331ad76f7214b97fe5479
KELSEY Bonnicksen <bonnicksen8017@students.cptc.edu>
2019-04-30 10:35:09 -0700

    Add project files.

---
+++ ExceptionsSample/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ExceptionsSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnValUsername = new System.Windows.Forms.Button();
+            this.Username = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(135, 264);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(182, 23);
+            this.btnValidate.TabIndex = 0;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter your age:";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(226, 117);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(360, 120);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(28, 13);
+            this.errorLabel.TabIndex = 4;
+            this.errorLabel.Text = "       ";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(226, 173);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // btnValUsername
+            // 
+            this.btnValUsername.Location = new System.Drawing.Point(363, 173);
+            this.btnValUsername.Name = "btnValUsername";
+            this.btnValUsername.Size = new System.Drawing.Size(158, 23);
+            this.btnValUsername.TabIndex = 6;
+            this.btnValUsername.Text = "Validate Username";
+            this.btnValUsername.UseVisualStyleBackColor = true;
+            this.btnValUsername.Click += new System.EventHandler(this.btnValUsername_Click);
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Location = new System.Drawing.Point(135, 179);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(61, 13);
+            this.Username.TabIndex = 7;
+            this.Username.Text = "Username: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.btnValUsername);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnValidate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnValUsername;
+        private System.Windows.Forms.Label Username;
+    }
+}
+
---
+++ ExceptionsSample/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExceptionsSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            if (isValidAge(txtAge.Text))
+            {
+                errorLabel.Text = "Thanks for the valid age!";
+            }
+            else
+            {
+                errorLabel.Text = "This is not a valid age!";
+            }
+        }
+
+        private bool IsValidByte(string input, Label errorLabel)
+        {
+            try
+            {
+                Convert.ToByte(input);
+                return true;
+            }
+            catch (Exception)
+            {
+                errorLabel.Text = "must be betweeen 0 and 255";
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Ensure username is not one of the blacklisted names
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        /// <param name="userName">The username to check</param>
+        /// <returns></returns>
+        private bool isValidUsername(String userName)
+        {
+            try
+            {
+                if (userName.ToLower() == "admin")
+                {
+                    throw new Exception("Invalid username!");
+                }
+            }
+            catch (Exception ex)
+            {
+                //log the exception to a text file          ??why would you do this??
+                MessageBox.Show("Invalid input");
+                throw ex;
+            }           
+            return true;
+        }
+
+        private bool isValidAge(string input)
+        {
+            try
+            {
+                //can use this instead of the if/else statement
+                byte age = Convert.ToByte(input);
+                if(age < 12 || age > 125)
+                {
+                    throw new OverflowException("Your age is not valid");
+                }
+                return true;
+                
+                //if(age >= 12 && age <= 125)
+                //{
+                //    return true;
+                //}
+                //return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void btnValUsername_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isValidUsername(txtUsername.Text))
+                {
+                    MessageBox.Show("Username is valid!");
+                }
+                else
+                {
+                    MessageBox.Show("Pick a different name!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("That username CANNOT be used!");
+            }
+        }
+
+        /// <summary>
+        /// Checks if a textbox contains text
+        /// </summary>
+        /// <param name="box">The textbox that we are checking</param>
+        /// <param name="name">A human-readable name for the textbox</param>
+        /// <returns></returns>
+        private bool isPresent(TextBox box, string name)
+        {
+            //if(box.Text.Trim() == string.Empty)
+            if(string.IsNullOrWhiteSpace(box.Text))
+            {
+                MessageBox.Show($"{name} is required!", 
+                    "ERROR",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                box.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="box">Textbox that you are pulling information out of</param>
+        /// <param name="name">Readable name given to the textbox</param>
+        /// <param name="min">The inclusive minimum value</param>
+        /// <param name="max">The inclusive maximum value</param>
+        /// <returns></returns>
+        private bool isWithinRange(TextBox box, string name, 
+                                   double min, double max)
+        {
+            //What you put when you do not habe any code to go inside of it
+            throw new NotImplementedException();
+        }
+
+        private bool IsFormDataValid()
+        {
+            if(isPresent(txtAge, "Age") && isPresent(txtUsername, "Username") &&
+                isValidAge(txtAge.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+
